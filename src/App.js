b549f58cde0a3b137e411c2473c0a6d6cf127816
HimuHimu112002 <mdhmaktaruzzaman9101@gmail.com>
2023-03-06 09:28:10 +0600

    crud

---
+++ src/App.js
@@ -0,0 +1,128 @@
+import { useEffect, useState } from 'react';
+import { Container, Form, ListGroup, Modal, Row, Col } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { getDatabase, ref, set, push, onValue, remove, update} from "firebase/database";
+import './list.css'
+function App() {
+
+  const db = getDatabase();
+
+  let [FirstName, setFirstName] = useState();
+  let [uiShow, setUiShow] = useState([]);
+
+  // popup show koranor jonno
+  const [show, setShow] = useState(false);
+  
+  // for button id access
+  const [id, setId] = useState(false);
+
+  let firstName =(item)=>{
+    setFirstName(item.target.value)
+  }
+   
+  let handleClick=(value)=>{
+    value.preventDefault();
+    set(push(ref(db, 'crud')),{
+     fname:FirstName,
+    });
+  }
+
+  useEffect(()=>{
+    onValue(ref(db, 'crud'), (snapshot) => {
+      let arr = []
+      snapshot.forEach((item)=>{
+        let show = {
+          id: item.key,
+          name: item.val().fname,
+          
+        }
+        arr.push(show)
+      });
+      setUiShow(arr);
+      });
+  }, [])
+
+  let handleDelete = (id) =>{
+    remove(ref(db, 'crud/'+ id))
+  }
+
+  const handleShow = (id) => {
+    setShow(true);
+    setId(id)
+  }
+
+  const handleClose = () =>{
+    setShow(false);
+    update(ref(db, 'crud/' + id), {
+      fname:FirstName,
+    })
+  }
+
+return (
+    <>
+      <Container>
+        <Row className='row'> 
+          <Col md="6" className='shadow py-5 rounded'>
+
+              <h1 className='mt-5 text-center text-danger'>CRUD</h1>
+
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Input Your Name Here</Form.Label>
+                  <Form.Control onChange={firstName} type="text" placeholder="First Name" />
+                </Form.Group>
+
+                <Button onClick={handleClick} variant="success" type="submit">Submit</Button>
+
+              </Form>
+
+              <ListGroup className='mt-3'>
+                {uiShow.map((item) => (
+
+                  <ListGroup.Item >
+                  <Row>
+
+                    <Col md="6" className='nameFild'>
+                      {item.name}<br></br>
+                    </Col>
+
+                    <Col md="6">
+                      <div className='mt-2 Update_button-align'>
+                        <Button className='btn1' onClick={()=>handleShow(item.id)} variant="success">Update</Button>
+                        <Button onClick={()=>handleDelete(item.id)} variant="danger">Delete</Button>
+                      </div>
+                    </Col>
+
+                  </Row>           
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Updating Your Name</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form.Group className="mb-3" controlId="formBasictext">
+                  <Form.Label>Input Your's Value.</Form.Label>
+                  <Form.Control onChange={firstName} type="text"/>
+                </Form.Group>
+              </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="success" onClick={handleClose}>
+                    Update
+                  </Button>
+
+                  <Button className='bg-danger' variant="secondary" onClick={handleClose}>
+                    Cancel
+                  </Button>
+              </Modal.Footer>
+            </Modal>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmCNzsvP0PWm0Rms5iECooE_3HNCkYfwE",
+  authDomain: "crud-cd16a.firebaseapp.com",
+  projectId: "crud-cd16a",
+  storageBucket: "crud-cd16a.appspot.com",
+  messagingSenderId: "620418703558",
+  appId: "1:620418703558:web:c97a5fe5d09bf69841f7c7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig
